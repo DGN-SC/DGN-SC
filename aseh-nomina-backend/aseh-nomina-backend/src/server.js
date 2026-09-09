c6005f341c1a1ac5b91bec6b2c6833eb413bfe6f
DGN-SC <juan.escorza@hidalgo.gob.mx>
2026-09-09 16:14:17 -0600

    prueba 5

--- aseh-nomina-backend/aseh-nomina-backend/src/server.js
+++ aseh-nomina-backend/aseh-nomina-backend/src/server.js
@@ -13,7 +13,8 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://localhost:5500',
   'http://127.0.0.1:3000',
-  'http://localhost:3000'
+  'http://localhost:3000',
+  'https://dgn-sc.onrender.com'
 ];
 
 app.use(cors({
