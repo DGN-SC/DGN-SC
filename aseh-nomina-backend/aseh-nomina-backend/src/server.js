3fbe0198c0355b009f3f11512d84b81bb380b4ab
DGN-SC <juan.escorza@hidalgo.gob.mx>
2026-09-09 13:09:02 -0600

    prueba1

--- aseh-nomina-backend/aseh-nomina-backend/src/server.js
+++ aseh-nomina-backend/aseh-nomina-backend/src/server.js
@@ -7,7 +7,13 @@ const axios = require("axios");
 
 const app = express();
 
-
+app.use(cors({
+  origin: [
+    'https://dgn-sc.github.io',
+    'http://127.0.0.1:5500', // Puerto habitual si usas Live Server
+    'http://localhost:5500'
+  ]
+}));
 // ============================================================
 // CONFIGURACIÓN
 // ============================================================
